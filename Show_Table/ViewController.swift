a94988213fbd1cd719c9661ad1f4d09399b7d42b
ShahriarHossain <shishir01825602222@gmail.com>
2020-02-18 10:05:54 +0600

    newone

---
+++ Show_Table/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Show_Table
+//
+//  Created by apple on 2/17/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    var item = ["Apple", "Dell", "ASUS", "Lenovo","MSI"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = item[indexPath.row]
+        
+        return cell!
+        
+    }
+
+}
+
